498a188798b150787503a042bfbe45669bfcd315
SlickSaintNick <144679503+SlickSaintNick@users.noreply.github.com>
2023-12-22 21:59:14 +1030

    Added javascript loops to create grid.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,4 +1,18 @@
+let size = 4;
+
 const boxes = document.querySelectorAll(".box");
+const grid = document.querySelector(".grid");
+
+for (let i = 0; i < size; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    for (let i = 0; i < size; i++) {
+        const box = document.createElement('div')
+        box.classList.add('box');
+        row.appendChild(box);
+    }
+    grid.appendChild(row);
+}
 
 boxes.forEach(box => {
     box.addEventListener('mouseover', (event) => {
