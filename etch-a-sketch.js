a678a2f1babec854d877c4de4aa0c3ec3f389af6
SlickSaintNick <144679503+SlickSaintNick@users.noreply.github.com>
2023-12-23 06:58:37 +1030

    Add function to clear grid; adjust main function.
    Changed the order to wait for a size from the user before
    clearing the grid or doing any other actions.
    
    Clicking the button now prompts a single time and if a
    valid response is not received, does nothing. This allows
    the user to cancel out and maintain their grid.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,33 +1,37 @@
 // Initialize constants and get grid.
-const MAX_SIZE = 100;
+const MAX_SIZE = 200;
 SKETCH_COLOR = 'red';
 const grid = document.querySelector('.grid');
-
-// Get size from user, draw the grid, and set up etch-a-sketch.
 const btnSize = document.querySelector('.btnSize');
+
+// When the button is clicked, get size from user, clear and redraw the grid, 
+// and set up etch-a-sketch. If a valid size is not entered, do nothing.
 btnSize.addEventListener('click', () => {
-    clearGrid();
-    let size = getSize();
-    drawGrid(size);
-    handleBoxes();
+    const size = getSize();
+    if (size) {
+        clearGrid()
+        drawGrid(size);
+        handleBoxes();
+        return 1;
+    }
+    return 0;
 });
 
 
+// Clear the grid
 function clearGrid() {
-    // TODO - implement clear grid.
-    // https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+    grid.textContent = '';
     return;
 }
 
 
 function getSize() {
     // Get a number between 1 and max size from the user.
-    keepGoing = true;
-    while (keepGoing) {
-        let n = parseInt(prompt(`Enter a number to set size of grid (max ${MAX_SIZE}): `));
-        if (n !== NaN && n !== undefined && n > 0 && n <= MAX_SIZE) {
-            return n;
-        }
+    let n = parseInt(prompt(`Enter a number to set size of grid (max ${MAX_SIZE}): `));
+    if (n > 0 && n <= MAX_SIZE) {
+        return n;
+    } else {
+        return false;
     }
 }
 
@@ -43,7 +47,8 @@ function drawGrid(size) {
             row.appendChild(box);
         }
         grid.appendChild(row);
-    }    
+    }
+    return;    
 }
 
 
