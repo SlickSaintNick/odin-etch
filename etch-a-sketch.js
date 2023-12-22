d45846a129de956d2e510647975dae4b551ab150
SlickSaintNick <144679503+SlickSaintNick@users.noreply.github.com>
2023-12-23 07:18:59 +1030

    Color is now randomised using rgb.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -58,7 +58,15 @@ function handleBoxes() {
 
     boxes.forEach(box => {
         box.addEventListener('mouseleave', (event) => {
-            event.target.style.backgroundColor = SKETCH_COLOR;
+            event.target.style.backgroundColor = randomRGB();
         });
     });    
+}
+
+
+function randomRGB() {
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+    return `rgb(${red} ${green} ${blue})`;
 }
